9cab203df2b8a7d8ff990c65ce60b6d63c76601c
zyros-dev <zyros.dev@gmail.com>
2025-06-15 17:42:43 +1000

    ok, I have the syntax for how to do a select statement

--- src/pgx_lower/sql/basic.sql
+++ src/pgx_lower/sql/basic.sql
@@ -1,2 +1,5 @@
 LOAD 'pgx_lower.so';
 SELECT 'hello';
+CREATE TABLE test(id SERIAL);
+INSERT INTO test(id) VALUES (2);
+SELECT * FROM test;
--- src/pgx_lower/src/my_executor.cpp
+++ src/pgx_lower/src/my_executor.cpp
@@ -1,8 +1,18 @@
 #include "my_executor.h"
-
 #include "executor/executor.h"
-#include "executor_c.h"
+
+extern "C" {
+#include "postgres.h"
+#include "executor/tuptable.h"
+#include "utils/snapmgr.h"
+#include "access/htup_details.h"
+#include "utils/lsyscache.h"
+#include "catalog/pg_type.h"
 #include "utils/elog.h"
+#include "access/table.h"
+#include "access/heapam.h"
+#include "utils/rel.h"
+}
 
 bool MyCppExecutor::execute(const QueryDesc* plan) {
     if (!plan) {
@@ -13,7 +23,54 @@ bool MyCppExecutor::execute(const QueryDesc* plan) {
     elog(NOTICE, "Inside C++ executor! Plan type: %d", plan->operation);
     elog(NOTICE, "Query text: %s", plan->sourceText ? plan->sourceText : "NULL");
 
-    // TODO: Implement actual query execution logic here
-    // For now just return false to indicate not handled
-    return false;
+    if (plan->operation != CMD_SELECT) {
+        elog(NOTICE, "Not a SELECT statement, skipping");
+        return false;
+    }
+
+    PlannedStmt *stmt = plan->plannedstmt;
+    Plan *rootPlan = stmt->planTree;
+
+    if (rootPlan->type != T_SeqScan) {
+        elog(NOTICE, "Only simple table scans (SeqScan) are supported in raw mode.");
+        return false;
+    }
+
+    SeqScan *scan = (SeqScan *)rootPlan;
+    RangeTblEntry *rte = (RangeTblEntry *)list_nth(stmt->rtable, scan->scan.scanrelid - 1);
+    Relation rel = table_open(rte->relid, AccessShareLock);
+
+    TableScanDesc scanDesc = table_beginscan(rel, GetActiveSnapshot(), 0, NULL);
+    TupleDesc tupdesc = RelationGetDescr(rel);
+    HeapTuple tuple;
+
+    // Print column names
+    for (int i = 0; i < tupdesc->natts; i++) {
+        Form_pg_attribute attr = TupleDescAttr(tupdesc, i);
+        elog(NOTICE, "Column %d: %s", i + 1, NameStr(attr->attname));
+    }
+
+    // Print rows
+    while ((tuple = heap_getnext(scanDesc, ForwardScanDirection)) != NULL) {
+        for (int j = 0; j < tupdesc->natts; j++) {
+            bool isNull;
+            Datum value = heap_getattr(tuple, j + 1, tupdesc, &isNull);
+            if (isNull) {
+                elog(NOTICE, "Column %d: NULL", j + 1);
+            } else {
+                Oid typeOutput;
+                bool typeIsVarlena;
+                getTypeOutputInfo(TupleDescAttr(tupdesc, j)->atttypid, &typeOutput, &typeIsVarlena);
+                char* str = OidOutputFunctionCall(typeOutput, value);
+                elog(NOTICE, "Column %d: %s", j + 1, str);
+                pfree(str);
+            }
+        }
+        elog(NOTICE, "---");
+    }
+
+    table_endscan(scanDesc);
+    table_close(rel, AccessShareLock);
+
+    return true;
 }
