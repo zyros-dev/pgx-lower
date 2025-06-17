12f7988f082ac15860644f3781e8c90cf043fa63
zyros-dev <zyros.dev@gmail.com>
2025-06-18 07:11:56 +1000

    some more progress on the debugger so its more useable

--- launch_postgres_gdbserver.sh
+++ launch_postgres_gdbserver.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+sudo pkill -u postgres postgres
+sudo lsof -t -i :1234 | xargs -r sudo kill -9
 sudo -u postgres gdbserver --debug localhost:1234 /usr/local/pgsql/bin/postgres -D /usr/local/pgsql/data
