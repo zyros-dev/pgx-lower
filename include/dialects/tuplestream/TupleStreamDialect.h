6e777890b75f53d067ec36e0210f6e7280835ce3
zyros-dev <zyros.dev@gmail.com>
2025-07-29 21:22:17 +1000

    updating... + some hash problem solved

--- include/dialects/tuplestream/TupleStreamDialect.h
+++ include/dialects/tuplestream/TupleStreamDialect.h
@@ -1,9 +1,25 @@
 #ifndef TUPLESTREAM_DIALECT_H
 #define TUPLESTREAM_DIALECT_H
+#include <memory>
 
+namespace llvm {
+class hash_code; // NOLINT (readability-identifier-naming)
+template <typename T>
+llvm::hash_code hash_value(std::shared_ptr<T> arg); // NOLINT (readability-identifier-naming)
+} // end namespace llvm
+#include "ColumnManager.h"
 #include "mlir/IR/Dialect.h"
 #include "Column.h"
-#include "ColumnManager.h"
+
+#ifndef MLIR_HASHCODE_SHARED_PTR
+#define MLIR_HASHCODE_SHARED_PTR
+namespace llvm {
+template <typename T>
+llvm::hash_code hash_value(std::shared_ptr<T> arg) { // NOLINT (readability-identifier-naming)
+   return hash_value(arg.get());
+}
+} // end namespace llvm
+#endif // MLIR_HASHCODE_SHARED_PTR
 
 namespace pgx_lower::compiler::dialect::tuples {
 class TupleStreamDialect;
