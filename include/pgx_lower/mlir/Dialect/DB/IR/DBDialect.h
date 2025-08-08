a57793868ebac3930c5613d0a4135fe7252e2ba8
zyros-dev <zyros.dev@gmail.com>
2025-08-09 08:03:35 +1000

    fix MLIR IR printing segfault - identify DB dialect type printing issue

--- include/pgx_lower/mlir/Dialect/DB/IR/DBDialect.h
+++ include/pgx_lower/mlir/Dialect/DB/IR/DBDialect.h
@@ -3,6 +3,8 @@
 
 #include "mlir/IR/Dialect.h"
 #include "mlir/IR/OpDefinition.h"
+#include "mlir/IR/DialectImplementation.h"
+#include "mlir/IR/Types.h"
 
 #include "mlir/Dialect/DB/IR/DBOpsDialect.h.inc"
 
