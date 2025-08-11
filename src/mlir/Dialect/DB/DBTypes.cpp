ea3ef00f27bb668b0844b8356646f2c3d82c46b1
zyros-dev <zyros.dev@gmail.com>
2025-08-11 11:51:47 +1000

    restore the db types file directly

--- src/mlir/Dialect/DB/DBTypes.cpp
+++ src/mlir/Dialect/DB/DBTypes.cpp
@@ -9,62 +9,62 @@
 
 namespace mlir {
 template <>
-struct FieldParser<pgx::mlir::db::DateUnitAttr> {
-   static FailureOr<pgx::mlir::db::DateUnitAttr> parse(AsmParser& parser) {
+struct FieldParser<mlir::db::DateUnitAttr> {
+   static FailureOr<mlir::db::DateUnitAttr> parse(AsmParser& parser) {
       llvm::StringRef str;
       if (parser.parseKeyword(&str)) return failure();
-      auto parsed = pgx::mlir::db::symbolizeDateUnitAttr(str);
-      if (parsed.has_value()) {
-         return parsed.value();
+      auto parsed = mlir::db::symbolizeDateUnitAttr(str);
+      if (parsed.hasValue()) {
+         return parsed.getValue();
       }
       return failure();
    }
 };
 
 template <>
-struct FieldParser<pgx::mlir::db::IntervalUnitAttr> {
-   static FailureOr<pgx::mlir::db::IntervalUnitAttr> parse(AsmParser& parser) {
+struct FieldParser<mlir::db::IntervalUnitAttr> {
+   static FailureOr<mlir::db::IntervalUnitAttr> parse(AsmParser& parser) {
       llvm::StringRef str;
       if (parser.parseKeyword(&str)) return failure();
-      auto parsed = pgx::mlir::db::symbolizeIntervalUnitAttr(str);
-      if (parsed.has_value()) {
-         return parsed.value();
+      auto parsed = mlir::db::symbolizeIntervalUnitAttr(str);
+      if (parsed.hasValue()) {
+         return parsed.getValue();
       }
       return failure();
    }
 };
 
 template <>
-struct FieldParser<pgx::mlir::db::TimeUnitAttr> {
-   static FailureOr<pgx::mlir::db::TimeUnitAttr> parse(AsmParser& parser) {
+struct FieldParser<mlir::db::TimeUnitAttr> {
+   static FailureOr<mlir::db::TimeUnitAttr> parse(AsmParser& parser) {
       llvm::StringRef str;
       if (parser.parseKeyword(&str)) return failure();
-      auto parsed = pgx::mlir::db::symbolizeTimeUnitAttr(str);
-      if (parsed.has_value()) {
-         return parsed.value();
+      auto parsed = mlir::db::symbolizeTimeUnitAttr(str);
+      if (parsed.hasValue()) {
+         return parsed.getValue();
       }
       return failure();
    }
 };
 
 namespace db {
-llvm::raw_ostream& operator<<(llvm::raw_ostream& os, const pgx::mlir::db::DateUnitAttr& dt) {
-   os << pgx::mlir::db::stringifyDateUnitAttr(dt);
+llvm::raw_ostream& operator<<(llvm::raw_ostream& os, const mlir::db::DateUnitAttr& dt) {
+   os << mlir::db::stringifyDateUnitAttr(dt);
    return os;
 }
-llvm::raw_ostream& operator<<(llvm::raw_ostream& os, const pgx::mlir::db::IntervalUnitAttr& dt) {
-   os << pgx::mlir::db::stringifyIntervalUnitAttr(dt);
+llvm::raw_ostream& operator<<(llvm::raw_ostream& os, const mlir::db::IntervalUnitAttr& dt) {
+   os << mlir::db::stringifyIntervalUnitAttr(dt);
    return os;
 }
-llvm::raw_ostream& operator<<(llvm::raw_ostream& os, const pgx::mlir::db::TimeUnitAttr& dt) {
-   os << pgx::mlir::db::stringifyTimeUnitAttr(dt);
+llvm::raw_ostream& operator<<(llvm::raw_ostream& os, const mlir::db::TimeUnitAttr& dt) {
+   os << mlir::db::stringifyTimeUnitAttr(dt);
    return os;
 }
 } // end namespace db
 } // end namespace mlir
 #define GET_TYPEDEF_CLASSES
 #include "mlir/Dialect/DB/IR/DBOpsTypes.cpp.inc"
-namespace pgx::mlir::db {
+namespace mlir::db {
 void DBDialect::registerTypes() {
    addTypes<
 #define GET_TYPEDEF_LIST
@@ -72,4 +72,4 @@ void DBDialect::registerTypes() {
       >();
 }
 
-} // namespace pgx::mlir::db
+} // namespace mlir::db
