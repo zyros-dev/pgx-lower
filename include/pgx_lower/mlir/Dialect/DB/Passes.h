644b51c4b6a76cf9e63cb7c4a698fa9d77b391b9
zyros-dev <zyros.dev@gmail.com>
2025-08-12 22:18:26 +1000

    fix: remove misplaced pipeline functions from DB/Passes.h

--- include/pgx_lower/mlir/Dialect/DB/Passes.h
+++ include/pgx_lower/mlir/Dialect/DB/Passes.h
@@ -3,17 +3,10 @@
 #include "mlir/Pass/Pass.h"
 
 namespace mlir {
-class OpPassManager;
-
 namespace db {
 std::unique_ptr<Pass> createEliminateNullsPass();
 std::unique_ptr<Pass> createSimplifyToArithPass();
 std::unique_ptr<Pass> createOptimizeRuntimeFunctionsPass();
-
-// Pipeline functions
-void createLowerDBPipeline(OpPassManager& pm);
-void registerDBConversionPasses();
-
 } // end namespace db
 } // end namespace mlir
 #endif // MLIR_DIALECT_DB_PASSES_H
