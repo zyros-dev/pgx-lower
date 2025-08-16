1156316cb8eab1d04cfd5e4d684f1678e4befbf0
zyros-dev <zyros.dev@gmail.com>
2025-08-17 04:22:35 +1000

    add a printer for relalg

---
+++ src/mlir/Dialect/RelAlg/RelAlgTypes.cpp
@@ -0,0 +1,26 @@
+#include "mlir/Dialect/RelAlg/IR/RelAlgTypes.h"
+#include "mlir/Dialect/RelAlg/IR/RelAlgDialect.h"
+
+#include "mlir/IR/Builders.h"
+#include "mlir/IR/DialectImplementation.h"
+
+#include <llvm/ADT/TypeSwitch.h>
+
+namespace mlir {
+namespace relalg {
+} // end namespace relalg
+} // end namespace mlir
+
+#define GET_TYPEDEF_CLASSES
+#include "mlir/Dialect/RelAlg/IR/RelAlgOpsTypes.cpp.inc"
+
+namespace mlir::relalg {
+
+void RelAlgDialect::registerTypes() {
+   addTypes<
+#define GET_TYPEDEF_LIST
+#include "mlir/Dialect/RelAlg/IR/RelAlgOpsTypes.cpp.inc"
+      >();
+}
+
+} // namespace mlir::relalg
