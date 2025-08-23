dc55d1108eb821f92047d6f06d19918fd9b9d782
zyros-dev <zyros.dev@gmail.com>
2025-08-23 15:38:37 +1000

    clean out what seems to be dead headers

--- include/lingodb/runtime/DecimalRuntime.h
+++
@@ -1,9 +0,0 @@
-#ifndef PGX_LOWER_RUNTIME_DECIMALRUNTIME_H
-#define PGX_LOWER_RUNTIME_DECIMALRUNTIME_H
-#include "lingodb/runtime/helpers.h"
-namespace pgx_lower::compiler::runtime {
-struct DecimalRuntime {
-   static __int128 round(__int128 value, int64_t digits, int64_t scale);
-};
-} // namespace pgx_lower::compiler::runtime
-#endif // PGX_LOWER_RUNTIME_DECIMALRUNTIME_H
