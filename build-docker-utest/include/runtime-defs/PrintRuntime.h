aa3180c59b1409e9521e1fd75de91f674e7777ab
zyros-dev <zyros.dev@gmail.com>
2026-04-19 14:27:49 +1000

    gitignore build-docker-utest + remove accidentally-tracked files

--- build-docker-utest/include/runtime-defs/PrintRuntime.h
+++
@@ -1,8 +0,0 @@
-#include <lingodb/mlir/Dialect/util/FunctionHelper.h>
-namespace rt {
-struct PrintRuntime {
- inline static mlir::util::FunctionSpec print =  mlir::util::FunctionSpec("runtime::PrintRuntime::print", "_ZN7runtime12PrintRuntime5printEPKc",  [](mlir::MLIRContext* context) { return std::vector<mlir::Type>{mlir::util::RefType::get(context,mlir::IntegerType::get(context,8))};}, [](mlir::MLIRContext* context) { return std::vector<mlir::Type>{};},false);
- inline static mlir::util::FunctionSpec printVal =  mlir::util::FunctionSpec("runtime::PrintRuntime::printVal", "_ZN7runtime12PrintRuntime8printValEPvi",  [](mlir::MLIRContext* context) { return std::vector<mlir::Type>{mlir::util::RefType::get(context,mlir::IntegerType::get(context,8)),mlir::IntegerType::get(context,32)};}, [](mlir::MLIRContext* context) { return std::vector<mlir::Type>{};},false);
- inline static mlir::util::FunctionSpec printPtr =  mlir::util::FunctionSpec("runtime::PrintRuntime::printPtr", "_ZN7runtime12PrintRuntime8printPtrEPvii",  [](mlir::MLIRContext* context) { return std::vector<mlir::Type>{mlir::util::RefType::get(context,mlir::IntegerType::get(context,8)),mlir::IntegerType::get(context,32),mlir::IntegerType::get(context,32)};}, [](mlir::MLIRContext* context) { return std::vector<mlir::Type>{};},false);
-};
-}
