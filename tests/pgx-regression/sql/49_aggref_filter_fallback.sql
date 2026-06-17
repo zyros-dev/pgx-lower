4f3c1f8637166e68cb39a54819d3d6d677b2eecd
zyros-dev <zyros.dev@gmail.com>
2026-06-17 13:10:53 +1000

    test(regression): normalize aggregate filter output

--- tests/pgx-regression/sql/49_aggref_filter_fallback.sql
+++ tests/pgx-regression/sql/49_aggref_filter_fallback.sql
@@ -5,8 +5,10 @@ CREATE TABLE aggref_filter_fallback(a int4);
 INSERT INTO aggref_filter_fallback VALUES (1), (1), (2);
 
 SET pgx_lower.execution_mode = 'auto';
+\pset format unaligned
 /* <<pgx-lower-config>>: auto_should_route_to=fallback id=pgx_49_aggref_filter_fallback_001 */
 SELECT count(*) FILTER (WHERE a = 1) AS filtered_count
 FROM aggref_filter_fallback;
+\pset format aligned
 
 DROP TABLE aggref_filter_fallback;
